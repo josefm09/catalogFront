188f63dba6758aa55e8f6980bcc66ba8034783cf
José Carlos Flores Moran <15171123@itculiacan.edu.mx>
2021-04-20 00:33:32 -0600

    ssl

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://jcflores.laziness.rocks:8080",
+  baseURL: "https://jcflores.laziness.rocks:8080",
   headers: {
     "Content-type": "application/json"
   }
